9472ec129e2735be3a8b806e538c73acba89ecf2
parkchanwong <cksdnd5433@gmail.com>
2026-08-27 18:58:56 +0900

    feat: JDBC getConnection, close, commit, rollback static method 추가

--- src/main/java/com/quotehunters/quotecollection/common/JDBC.java
+++ src/main/java/com/quotehunters/quotecollection/common/JDBC.java
@@ -1,4 +1,90 @@
 package com.quotehunters.quotecollection.common;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class JDBC {
+    public static Connection getConnection() {
+        Connection connection = null;
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileReader("src/main/java/com/quotehunters/quotecollection/config/connection-info.properties"));
+            String driver = properties.getProperty("driver");
+            String url = properties.getProperty("url");
+
+            Class.forName(driver);
+
+            connection = DriverManager.getConnection(url, properties);
+
+            connection.setAutoCommit(false);
+
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static void close(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Statement statement) {
+        try {
+            if (statement != null && !statement.isClosed()) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(PreparedStatement preparedStatement) {
+        try {
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet resultSet) {
+        try {
+            if (resultSet != null && !resultSet.isClosed()) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void commit(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.commit();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rollback(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
