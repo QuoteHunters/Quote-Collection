98482f5d7baa5663c208e99248f61bdedceb74c2
suyoungbang <suyoungbang27@gmail.com>
2026-08-28 16:13:10 +0900

    feat: 전체 인물 조회 기능 구현

---
+++ src/main/java/com/quotehunters/quotecollection/model/service/PersonService.java
@@ -0,0 +1,25 @@
+package com.quotehunters.quotecollection.model.service;
+
+import com.quotehunters.quotecollection.common.JDBC;
+import com.quotehunters.quotecollection.model.dao.PersonDAO;
+import com.quotehunters.quotecollection.model.dto.PersonDTO;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class PersonService {
+    private PersonDAO personDAO = new PersonDAO();
+
+
+    // 전체 인물 조회
+    public List<PersonDTO> selectAllPerson() {
+
+        Connection con = JDBC.getConnection();
+
+        List<PersonDTO> personList = personDAO.selectAllPerson(con);
+
+        JDBC.close(con);
+
+        return personList;
+    }
+}
