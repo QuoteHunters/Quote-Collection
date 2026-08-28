c69fdb7c052bad885e6a0c9eef15804ffe0bdbd8
parkchanwong <cksdnd5433@gmail.com>
2026-08-28 15:31:02 +0900

    feat: 분야 선택이 가능한 기능 추가

--- src/main/java/com/quotehunters/quotecollection/view/FieldView.java
+++ src/main/java/com/quotehunters/quotecollection/view/FieldView.java
@@ -4,10 +4,13 @@ import com.quotehunters.quotecollection.controller.FieldController;
 import com.quotehunters.quotecollection.model.dao.FieldDTO;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FieldView {
-    private FieldController fieldController = new FieldController();
+    private final FieldController fieldController = new FieldController();
+    private final ScannerView scv = new ScannerView();
+    private final Scanner sc = new Scanner(System.in);
 
     public void allFields() {
         List<FieldDTO> fields = fieldController.allFields();
@@ -23,4 +26,44 @@ public class FieldView {
         }
         System.out.println("----------------------------");
     }
+
+    public int selectFields() {
+        List<FieldDTO> fields = fieldController.allFields();
+        int choice = 0;
+
+        while (true) {
+            choice = scv.scannInt(sc, "선택 (0: 뒤로가기)");
+            if (choice == 0) break;
+
+            if (fields.size() < choice) {
+                System.out.println("메뉴에 있는 번호를 선택해주세요");
+                continue;
+            }
+
+            break;
+        }
+
+        if (choice == 0) return choice;
+
+        return fields.get(choice - 1).getFieldId();
+    }
+
+//    public void modifyField(int id) {
+//        List<FieldDTO> fields = fieldController.allFields();
+//
+//        while (true) {
+//
+//
+//            String changeName = scv.scannString(sc, "변경할 분야명 입력 (0: 뒤로가기)");
+//            if (changeName.equals("0")) {
+//                continue;
+//            }
+//
+//
+////            System.out.println(field.getFieldName() + " => " + changeName);
+//            System.out.println("정말 수정하시겠습니까? 예 / 재수정 / 아니오");
+//
+//            return;
+//        }
+//    }
 }
